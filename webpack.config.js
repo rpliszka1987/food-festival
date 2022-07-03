8f0c203260d7047a70f999713febbbd8b94e20c1
Robert Pliszka <robplisz@gmail.com>
2022-07-03 14:52:16 -0400

    Created separate JS files to load for specific pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const webpack = require("webpack");
 const path = require("path");
 
@@ -11,6 +13,9 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
     }),
   ],
   mode: "development",
