2dcd842311f4bb3609151f65e0e77f7c39a16759
Robert Pliszka <robplisz@gmail.com>
2022-07-08 17:36:01 -0400

    Finished PWA setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const webpack = require("webpack");
-const path = require("path");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
 
-module.exports = {
+const config = {
   entry: {
     app: "./assets/js/script.js",
     events: "./assets/js/events.js",
@@ -15,22 +14,19 @@ module.exports = {
     path: path.join(__dirname + "/dist"),
     filename: "[name].bundle.js",
   },
-  // used to convert images
   module: {
     rules: [
       {
-        test: /\.jpg$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
             loader: "file-loader",
-            // changeto tthe file name we want
             options: {
               esModule: false,
               name(file) {
                 return "[path][name].[ext]";
               },
-              publicPath: function (url) {
-                // replace ../ with /assets/
+              publicPath(url) {
                 return url.replace("../", "/assets/");
               },
             },
@@ -48,28 +44,21 @@ module.exports = {
       jQuery: "jquery",
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+      analyzerMode: "static",
     }),
     new WebpackPwaManifest({
-      // Desktop name
       name: "Food Event",
-      // Mobile app name
       short_name: "Foodies",
-      description: "An app that allows you to view upcoming food events",
-      // Specify home page of pwa
+      description: "An app that allows you to view upcoming food events.",
       start_url: "../index.html",
       background_color: "#01579b",
-      theme_color: "#fff",
+      theme_color: "#ffffff",
       fingerprints: false,
       inject: false,
-      // Icons for app
       icons: [
         {
-          // Path to the images
           src: path.resolve("assets/img/icons/icon-512x512.png"),
-          // Icon sizes
           sizes: [96, 128, 192, 256, 384, 512],
-          // Where the icons will be send after creation
           destination: path.join("assets", "icons"),
         },
       ],
@@ -77,3 +66,5 @@ module.exports = {
   ],
   mode: "development",
 };
+
+module.exports = config;
